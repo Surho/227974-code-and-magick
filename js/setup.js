8adadbaceeec37c54e729bd2b6947b65485e7ce5
Surho <surho007@hotmail.com>
2017-04-04 00:23:21 +0300

    additional fixes

--- js/setup.js
+++ js/setup.js
@@ -1,75 +1,77 @@
 'use strict';
-;(function() {
+;(function (){
 
-function genereteNumber(min, max) {
-  var randomNumber = min + Math.random() * ((max + 1) - min);
-  return Math.floor(randomNumber);
-}
-
-function genereteWizardName () {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 
-                      'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-      WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 
-                         'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  return (Math.random() <= 0.5) ? WIZARD_NAMES[genereteNumber(0, WIZARD_NAMES.length-1)] + ' ' + WIZARD_SURNAMES[genereteNumber(0, WIZARD_SURNAMES.length-1)] :
-                                  WIZARD_SURNAMES[genereteNumber(0, WIZARD_SURNAMES.length-1)] + ' ' + WIZARD_NAMES[genereteNumber(0, WIZARD_NAMES.length-1)];
-}
-
-function genereteCoatColor () {
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 
-                   'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  return COAT_COLORS[genereteNumber(0, COAT_COLORS.length-1)];
-}
-
-function genereteEyeColor () {
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-  return EYES_COLORS[genereteNumber(0, EYES_COLORS.length-1)];
-}
-
-function genereteWizardTemplate (wizard) {
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content,
-      wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-  return wizardElement;
-}
-
-function genereteWizardsObject () {
-  var wizardsObject = [];
-  for (var i = 0; i < 4; i++) {
-    wizardsObject[i] = {
-      name: genereteWizardName(),
-      coatColor: genereteCoatColor(), 
-      eyesColor: genereteEyeColor()
-    };
-  };
-  return wizardsObject;
-};
-
-function genereteWizardElement () {
-  var wizardsObject = genereteWizardsObject(),
-      fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizardsObject.length; i++) {
-  fragment.appendChild(genereteWizardTemplate(wizardsObject[i]));
+  function genereteNumber (min, max){
+    var randomNumber = min + Math.random() * ((max + 1) - min);
+    return Math.floor(randomNumber);
   }
-  return fragment;
-}
 
-var setupElement = document.querySelector('.setup');
+  function genereteWizardName (){
+    var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф',
+                'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+        SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц',
+                    'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+        fullName=(Math.random() <= 0.5) ? NAMES[genereteNumber(0, NAMES.length - 1)] + ' ' + SURNAMES[genereteNumber(0, SURNAMES.length - 1)] :
+                                          SURNAMES[genereteNumber(0, SURNAMES.length - 1)] + ' ' + NAMES[genereteNumber(0, NAMES.length - 1)];
+    return fullName;
+  }
 
-function createWizards () {
-  var similarListElement = setupElement.querySelector('.setup-similar-list');
-  similarListElement.appendChild(genereteWizardElement());
-}
+  function genereteCoatColor (){
+    var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+                    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+        generetedColor = COAT_COLORS[genereteNumber(0, COAT_COLORS.length - 1)];
+    return generetedColor;
+  }
 
-function showSetup () {
-  var setupElement = document.querySelector('.setup');
-  setupElement.classList.remove('hidden');
-  setupElement.querySelector('.setup-similar').classList.remove('hidden'); 
-}
+  function genereteEyeColor (){
+    var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'],
+        generetedColor = EYES_COLORS[genereteNumber(0, EYES_COLORS.length - 1)];
+    return generetedColor;
+  }
 
-createWizards();
-showSetup();
+  function genereteWizardTemplate (wizard){
+    var similarWizardTemplate = document.querySelector('#similar-wizard-template').content,
+        wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    return wizardElement;
+  }
+
+  function genereteWizardsObject (){
+    var wizardsObject = [];
+    for (var i = 0; i < 4; i++) {
+      wizardsObject[i] = {
+        name: genereteWizardName (),
+        coatColor: genereteCoatColor (),
+        eyesColor: genereteEyeColor ()
+      }
+    }
+    return wizardsObject;
+  };
+
+  function genereteWizardElement (){
+    var wizardsObject = genereteWizardsObject (),
+        fragment = document.createDocumentFragment ();
+    for (var i = 0; i < wizardsObject.length; i++) {
+    fragment.appendChild(genereteWizardTemplate(wizardsObject[i]));
+    }
+    return fragment;
+  }
+
+  function createWizards () {
+    var setupElement = document.querySelector('.setup'),
+        similarListElement = setupElement.querySelector('.setup-similar-list');
+    similarListElement.appendChild(genereteWizardElement());
+  }
+
+  function showSetup () {
+    var setupElement = document.querySelector('.setup');
+    setupElement.classList.remove('hidden');
+    setupElement.querySelector('.setup-similar').classList.remove('hidden');
+  }
+
+  createWizards ();
+  showSetup ();
 
 })();
--- js/stat.js
+++ js/stat.js
@@ -1,39 +1,39 @@
 'use strict';
-window.renderStatistics=function(ctx, names, times) {
-  ctx.fillStyle="rgba(0, 0, 0, 0.7)";
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle="white";
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle="#000";
-  ctx.fillText('Ура, вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+window.renderStatistics = function (ctx, names, times){
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fillRect (110, 20, 420, 270);
+  ctx.fillStyle = "white";
+  ctx.fillRect (100, 10, 420, 270);
+  ctx.fillStyle = "#000";
+  ctx.fillText ('Ура, вы победили!', 120, 40);
+  ctx.fillText ('Список результатов:', 120, 60);
 
-  var max=0;
+  var max = 0;
   var maxIndex;
-  for (var i=0; i<times.length; i++) {
-    if (times[i]>max) {
-      max=times[i];
-      maxIndex=i;
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > max) {
+      max = times[i];
+      maxIndex = i;
     }
   };
 
-  var histagramHeight=150;
-  var columnWidth=40;
-  var spaceBetween=50;
-  var initialX=140;
-  var initialY=250;
-  var step=histagramHeight/max;
+  var histagramHeight = 150;
+  var columnWidth = 40;
+  var spaceBetween = 50;
+  var initialX = 140;
+  var initialY = 250;
+  var step = histagramHeight / max;
 
-  for (i=0; i<times.length; i++) {
-    var timeRounded=Math.floor(times[i]);
-    var columnOpacity=(Math.ceil(Math.random()*10))/10;
-    ctx.fillStyle='rgba(2, 14, 134,'+ columnOpacity + ')'
+  for (i = 0; i < times.length; i++) {
+    var timeRounded = Math.floor(times[i]);
+    var columnOpacity = (Math.ceil(Math.random()*10))/10;
+    ctx.fillStyle = 'rgba(2, 14, 134,'+ columnOpacity + ')'
     if (names[i]==="Вы") {
-      ctx.fillStyle="red";
+      ctx.fillStyle = "red";
     }
-    ctx.fillRect(initialX+i*(spaceBetween+columnWidth), initialY-times[i]*step, columnWidth, times[i]*step);
-    ctx.fillStyle="#000";
-    ctx.fillText(names[i], initialX+i*(spaceBetween+columnWidth), initialY+20);
-    ctx.fillText(timeRounded, initialX+i*(spaceBetween+columnWidth), (initialY-times[i]*step)-20);
+    ctx.fillRect( initialX+i*(spaceBetween+columnWidth), initialY-times[i]*step, columnWidth, times[i]*step);
+    ctx.fillStyle = "#000";
+    ctx.fillText (names[i], initialX+i*(spaceBetween+columnWidth), initialY+20);
+    ctx.fillText (timeRounded, initialX+i*(spaceBetween+columnWidth), (initialY-times[i]*step)-20);
   }
 };
